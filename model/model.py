517a9c6c5f8d668a42b6fbd7a330b03b4af993c6
Hao <s1786991@sms.ed.ac.uk>
2019-01-30 23:10:07 +0000

    finish image captioning model and data

--- model/model.py
+++ model/model.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
 from base import BaseModel
 
 
@@ -20,3 +22,131 @@ class MnistModel(BaseModel):
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+
+class EncoderCNN(BaseModel):
+    """
+    Encoder
+    """
+
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+
+        resnet = torchvision.models.resnet101(pretrained=True)
+
+        # Remove linear and pool layers
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+
+        # Resize image to fixed size to allow input images of variable size
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        self.fine_tune()
+
+    def forward(self, images):
+        """
+        Forward propagation.
+
+        :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
+        :return: encoded images
+        """
+
+        out = self.resnet(images)  # (batch_size, 2048, image_size/32, image_size/32)
+        out = out.view(out.size(0), -1) # (batch_size, 2048 * image_size/32 * image_size/32)
+        out = self.embed(out)  # (batch_size, embed_size)
+        out = self.bn(out)
+
+        # out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 2048)
+        return out
+
+    def fine_tune(self, fine_tune=True):
+        """
+        Allow or prevent the computation of gradients for convolutional blocks 2 through 4 of the encoder.
+
+        :param fine_tune:
+        """
+
+        for p in self.resnet.parameters():
+            p.requires_grad = False
+        # If fine-tuning, only fine tune convolutional blocks 2 through 4
+        for c in list(self.resnet.children())[5:]:
+            for p in c.parameters():
+                p.requires_grad = fine_tune
+
+
+class DecoderRNN(BaseModel):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        """
+        Set the hyper-parameters and build the layers.
+
+        :param embed_size: word embedding size
+        :param hidden_size: hidden unit size of LSTM
+        :param vocab_size: size of vocabulary (output of the network)
+        :param num_layers: LSTM layers
+        """
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, features, captions):
+        """ Decode image feature vectors and generate captions. """
+        captions = captions[:,:-1]
+        embeddings = self.embed(captions)
+        inputs = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(inputs)
+        outputs = self.linear(hiddens)
+
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        """Accept a pre-processed image tensor (inputs) and return predicted
+        sentence (list of tensor ids of length max_len). This is the greedy
+        search approach.
+        """
+        sampled_ids = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            # Get the index (in the vocabulary) of the most likely integer that
+            # represents a word
+            predicted = outputs.argmax(1)
+            sampled_ids.append(predicted.item())
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        return sampled_ids
+
+    def sample_beam_search(self, inputs, states=None, max_len=20, beam_width=5):
+        """Accept a pre-processed image tensor and return the top predicted
+        sentences. This is the beam search approach.
+        """
+        # Top word idx sequences and their corresponding inputs and states
+        idx_sequences = [[[], 0.0, inputs, states]]
+        for _ in range(max_len):
+            # Store all the potential candidates at each step
+            all_candidates = []
+            # Predict the next word idx for each of the top sequences
+            for idx_seq in idx_sequences:
+                hiddens, states = self.lstm(idx_seq[2], idx_seq[3])
+                outputs = self.linear(hiddens.squeeze(1))
+                # Transform outputs to log probabilities to avoid floating-point
+                # underflow caused by multiplying very small probabilities
+                log_probs = F.log_softmax(outputs, -1)
+                top_log_probs, top_idx = log_probs.topk(beam_width, 1)
+                top_idx = top_idx.squeeze(0)
+                # create a new set of top sentences for next round
+                for i in range(beam_width):
+                    next_idx_seq, log_prob = idx_seq[0][:], idx_seq[1]
+                    next_idx_seq.append(top_idx[i].item())
+                    log_prob += top_log_probs[0][i].item()
+                    # Indexing 1-dimensional top_idx gives 0-dimensional tensors.
+                    # We have to expand dimensions before embedding them
+                    inputs = self.embed(top_idx[i].unsqueeze(0)).unsqueeze(0)
+                    all_candidates.append([next_idx_seq, log_prob, inputs, states])
+            # Keep only the top sequences according to their total log probability
+            ordered = sorted(all_candidates, key=lambda x: x[1], reverse=True)
+            idx_sequences = ordered[:beam_width]
+        return [idx_seq[0] for idx_seq in idx_sequences]
+
+
+
