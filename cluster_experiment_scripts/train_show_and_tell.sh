3ac11c364bfa47b45b0474a32505050824705520
Cuijing CHEN <2714582899@qq.com>
2019-02-11 11:44:19 +0000

    add sbatch script

--- cluster_experiment_scripts/train_show_and_tell.sh
+++ cluster_experiment_scripts/train_show_and_tell.sh
@@ -3,14 +3,14 @@
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Standard
 #SBATCH --gres=gpu:8
-#SBATCH --mem=12000  # memory in Mb
+#SBATCH --mem=48000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
 export CUDNN_HOME=/opt/cuDNN-7.0/
 
-export STUDENT_ID=s1786991
+export STUDENT_ID=s1784380
 
 export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 
@@ -32,6 +32,6 @@ mkdir -p ${TMP}/datasets/
 export DATASET_DIR=${TMP}/datasets/
 # Activate the relevant virtual environment:
 
-source /home/${STUDENT_ID}/anaconda3/bin/activate I2T2I
+source /home/${STUDENT_ID}/miniconda3/bin/activate I2T2I
 cd ..
-python train.py
+python train.py 
