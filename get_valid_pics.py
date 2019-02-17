29b1dbe65c5ccb02077218239091e51ee63b76bc
Cuijing CHEN <2714582899@qq.com>
2019-02-17 12:47:32 +0000

    s

--- get_valid_pics.py
+++ get_valid_pics.py
@@ -1,19 +1,21 @@
-
-from PIL import Image
+from trainer.trainer_cls import Trainer
 from torch.utils.data import DataLoader
 from data_loader.txt2image_dataset import Text2ImageDataset_Origin
+import argparse
+from PIL import Image
+import os
 
-
-
+from utils.arg_extractor import get_args
 
 if __name__ == '__main__':
+    # args = get_args()
 
-    valid_data_loader = DataLoader(
+    train_data_loader = DataLoader(
 
         Text2ImageDataset_Origin(
             data_dir='/home/s1784380/I2T2I/data/',
-            dataset_name="flowers",
-            which_set="valid"
+            dataset_name="birds",
+            which_set="train"
         ),
 
         batch_size=64,
@@ -21,12 +23,16 @@ if __name__ == '__main__':
         num_workers=0
     )
 
-    for batch_idx, sample in enumerate(valid_data_loader):
-        if batch_idx != 0:
-            break
-        right_images = sample['right_images']
-        txt = sample['txt']
+    trainer = Trainer(gan_type="gan_cls",
+                      train_data_loader=train_data_loader,
+                      valid_data_loader=train_data_loader,
+                      num_epochs=0,
+                      lr=0,
+                      save_path="/home/s1784380/lala/I2T2I",
+                      l1_coef=0,
+                      l2_coef=0,
+                      pre_trained_disc="/home/s1784380/lala/I2T2I/saved/gan_cls/l1nl2/birds/checkpoints/disc_185.pth",
+                      pre_trained_gen="/home/s1784380/lala/I2T2I/saved/gan_cls/l1nl2/birds/checkpoints/gen_185.pth",
+                      )
 
-        for image, t in zip(right_images, txt):
-            im = Image.fromarray(image.data.mul_(127.5).add_(127.5).byte().permute(1, 2, 0).cpu().numpy())
-            im.save("/home/s1784380/lala/I2T2I/valid_pic/flowers/ß{}.jpg".format(t.replace("/", "")[:100]))
+    trainer.predict(data_loader=train_data_loader,valid=True,epoch=185)
