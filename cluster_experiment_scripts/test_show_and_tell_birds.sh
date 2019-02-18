ec633691e3c4df7d4667688707b7e56866a0404b
Hao Chen <s1786991@greekie.inf.ed.ac.uk>
2019-02-18 10:50:24 +0000

    test sh files

---
+++ cluster_experiment_scripts/test_show_and_tell_birds.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Short
+#SBATCH --gres=gpu:2
+#SBATCH --mem=48000  # memory in Mb
+#SBATCH --time=0-04:00:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=s1786991
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets/
+# Activate the relevant virtual environment:
+
+source /home/${STUDENT_ID}/anaconda3/bin/activate I2T2I
+cd ..
+python test.py -r /home/s1786991/I2T2I/saved/Show-and-Tell-Birds/0212_192726/model_best.pth
