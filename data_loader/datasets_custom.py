7a267b9e96cced34e8742b4235a40c0fc50e0598
Hao <s1786991@sms.ed.ac.uk>
2019-03-15 11:31:36 +0000

    fix coco path bug

--- data_loader/datasets_custom.py
+++ data_loader/datasets_custom.py
@@ -85,9 +85,9 @@ class COCOTextImageDataset(Dataset):
         wrong_embed = 0
 
         # Processing images
-        right_image = Image.open(os.path.join(self.data_dir + 'images/{}/'.format(self.which_set), right_image_path))
+        right_image = Image.open(os.path.join(self.data_dir + '/images/{}/'.format(self.which_set), right_image_path))
         right_image = right_image.convert("RGB")
-        wrong_image = Image.open(os.path.join(self.data_dir + 'images/{}/'.format(self.which_set), wrong_image_path))
+        wrong_image = Image.open(os.path.join(self.data_dir + '/images/{}/'.format(self.which_set), wrong_image_path))
         wrong_image = wrong_image.convert("RGB")
 
         right_image_32 = right_image.resize((32, 32))
