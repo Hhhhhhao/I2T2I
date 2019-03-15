b28500cd2b0161c2bf0593b84f54ca8b5b3cfe6d
Hao <s1786991@sms.ed.ac.uk>
2019-03-15 11:43:16 +0000

    data pth

--- data_loader/datasets_custom.py
+++ data_loader/datasets_custom.py
@@ -35,8 +35,7 @@ class COCOTextImageDataset(Dataset):
             @:param datasetFile (string): path for dataset file
             @:param which_set (string): "train:, "valid", "test"
         """
-
-        if '/' not in data_dir:
+        if data_dir[-1] != '/':
             data_dir += '/'
         self.data_dir = data_dir
         self.which_set = which_set
