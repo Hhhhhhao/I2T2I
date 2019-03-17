0ffdf161075bff6e73b52b2b9fbd8bfd029d2f9b
Hao <s1786991@sms.ed.ac.uk>
2019-03-17 01:07:03 +0000

    change to 1 gpu

--- cluster_experiment_scripts/train_caption_gan_flowers.sh
+++ cluster_experiment_scripts/train_caption_gan_flowers.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Standard
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:1
 #SBATCH --mem=72000  # memory in Mb
 #SBATCH --time=0-08:00:00
 
